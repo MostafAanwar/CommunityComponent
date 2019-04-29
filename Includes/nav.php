b6290944578c6bd489db89efbf567a6f5cd7400c
Mostafa Anwar <mostafa.anwar12437@hotmail.com>
2019-04-29 14:29:18 +0200

    updated the nav.php file
    changed href of one of the buttons

--- Includes/nav.php
+++ Includes/nav.php
@@ -4,7 +4,7 @@
     <a href="">logout</a>
     <a href="">Inbox</a>
     <a href="">Settings</a>
-        <a href="../community/newsFeed.php">Explore</a>
+        <a href="../newsFeed.php">News feed</a>
 </div>
     <a href="index.php">Home</a>
     <div id='logo'><h1>Community</h1>
